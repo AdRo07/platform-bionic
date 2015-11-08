4c2b44002b3b6fce26b700e283927f6c6b827ab7
timduru <timduru@gmail.com>
2015-11-08 18:01:33 +0100

    Reenable support for non-PIE executables

--- linker/Android.mk
+++ linker/Android.mk
@@ -64,6 +64,11 @@ LOCAL_STATIC_LIBRARIES := libc_nomalloc libziparchive libutils libz liblog
 
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
+ifeq ($(TARGET_NEEDS_NON_PIE_SUPPORT),true)
+  LOCAL_CFLAGS += -DENABLE_NON_PIE_SUPPORT
+endif
+
+
 LOCAL_MODULE := linker
 LOCAL_MODULE_STEM_32 := linker
 LOCAL_MODULE_STEM_64 := linker64
